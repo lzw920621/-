ce5544039719ea32228fe4505c9b79f9a4d9dc1a
lzw920621 <1010616217@qq.com>
2019-07-12 22:18:43 +0800

    更新

--- 计数排序/Program.cs
+++ 计数排序/Program.cs
@@ -10,6 +10,42 @@ namespace 计数排序
     {
         static void Main(string[] args)
         {
+            int[] array = new int[] { 9, 2, 4, 1, 5, 2, 8, 7, -1, 20, 11, 6 };
+            CountingSort(array);
         }
+
+        public static void CountingSort(int[] array)
+        {
+            int min = array[0], max = array[0];
+            for (int i = 0; i < array.Length; i++)//寻找最大和最小值
+            {
+                if(array[i]<min)
+                {
+                    min = array[i];                     
+                }
+                if(array[i]>max)
+                {
+                    max = array[i];
+                }
+            }
+
+            int[] newArray = new int[max - min + 1];//这个数组存放 array中每个元素出现的次数 索引为元素值减去min
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[array[i] - min]++;
+            }
+
+            int index = 0;
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                while(newArray[i]>0)
+                {
+                    array[index] = i + min;
+                    index++;
+                    newArray[i]--;
+                }
+            }
+        }
+
     }
 }
