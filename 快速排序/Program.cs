29c37fd979b5984a89a326fa0e7841a36548be37
lzw920621 <1010616217@qq.com>
2019-07-12 21:57:34 +0800

    更新

--- 快速排序/Program.cs
+++ 快速排序/Program.cs
@@ -6,7 +6,40 @@ namespace 快速排序
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //9, 2, 4, 1, 5, 2, 8, 7, -1, 20, 11, 6
+            QuickSort(new int[] { 9, 2, 4, 1, 5, 2, 8, 7, -1, 20, 11, 6 });
         }
+
+        public static void QuickSort(int[] array)
+        {
+            Partition(array, 0, array.Length - 1);            
+        }
+        static void Partition(int[] array, int left, int right)
+        {
+            if (left >= right) return;
+
+            int pivot = array[left];
+            int l = left, r = right;
+            while(l<r)
+            {
+                while(l<r && array[r] >= pivot)
+                {
+                    r--;
+                }
+                array[l] = array[r];
+                while(l<r && array[l] <= pivot)
+                {
+                    l++;
+                }
+                array[r] = array[l];
+            }
+            array[l] = pivot;
+
+            Partition(array, left, l - 1);
+            Partition(array, l + 1, right);
+        }
+        
     }
+
+    
 }
--- 选择排序/Program.cs
+++ 选择排序/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace 选择排序
 {
+    /*
+    首先在未排序序列中找到最小元素，存放到排序序列的起始位置。
+    再从剩余未排序元素中继续寻找最小元素，然后放到已排序序列的末尾。
+    重复第二步，直到所有元素均排序完毕。
+    */
     class Program
     {
         static void Main(string[] args)
@@ -21,20 +26,27 @@ namespace 选择排序
 
         static int[] SelectSort(int[] array)
         {
-            int index = 0;
-            for (int i = 0; i < array.Length; i++)
+            int index = 0;//每次找到的 最小值 的索引
+            int temp;//临时值 存储每一次找到的最小值
+
+            
+            for (int i = 0; i < array.Length-1; i++)//i表示 未排序 序列的起始索引(同时也是 已排序序列 的末尾索引)
             {
-                int temp = int.MaxValue;                
-                for (int j = i; j < array.Length; j++)
+                temp = array[i];//temp初始值设为未排序序列的第一个元素值                
+                for (int j = i+1; j < array.Length; j++)//从未排序序列中找一个最小值的索引
                 {
                     if(array[j]<temp)
-                    {
-                        temp = array[j];
-                        index = j;
+                    {                        
+                        index = j;//最小值的索引
                     }
                 }
-                array[index] = array[i];
-                array[i] = temp;
+                if(i!=index)
+                {
+                    //交换找到的最小值 和 array[i]
+                    temp = array[index];
+                    array[index] = array[i];
+                    array[i] = temp;
+                }                
             }
             return array;
         }
